48a50d569a7ecedfc791a9a55ce27eabdf4d44fa
firefly-Dream-J <firefly@LAPTOP-NJLA9BI5.>
2026-01-30 19:22:01 +0800

    更新博客内容和功能

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,14 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+    site: 'https://example.com',
+    integrations: [mdx(), sitemap(), icon()],
+    devToolbar: {
+        enabled: false
+   },
+  
 });
